9e23220ab83bc96f7e439abc389255be4e48672c
Ksenia Busquet <ksenia.busquet@gmail.com>
2022-05-13 17:21:45 -0300

    Iteration 1 - w/out date

---
+++ src/components/IdCard.js
@@ -0,0 +1,23 @@
+import {format} from "date-fns";
+
+function IdCard(props) {
+    console.log(props);
+   
+    return (
+      <div className = "ID-card-wrapper">
+          <img src = {props.picture} alt = "profile"/>
+          
+            <p>
+              <b>First name: </b>{props.firstName} <br />
+              <b>Last name: </b>{props.lastName} <br />
+              <b>Gender: </b>{props.gender} <br />
+              <b>Height: </b>{props.height} <br />
+              {/*<b>Birth: </b>{format}({props.birth}, "MMMM do, yyyy") <br />*/}
+            </p>
+            
+          
+      </div>
+    );
+  }
+   
+  export default IdCard;
